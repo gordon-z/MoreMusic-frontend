7ded13f442dc94e627d7e8bd154ced50ff9912b6
gordon-z <gordon-zhang@hotmail.com>
2024-10-24 01:09:35 -0400

    added temp icon, redirect for github pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,6 +3,9 @@ function NotFound() {
         <div>
             <h1>404 Not Found</h1>
             <p>The page that you're looking for does not exist!</p>
+            <Link to="/" className="w-full bg-lime-600 hover:bg-lime-700 active:bg-lime-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-green-300">
+                Go Back to Home
+            </Link>
         </div>
     );
 }
